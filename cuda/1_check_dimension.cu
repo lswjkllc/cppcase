21c6fcfe0aa4b6e75acc672271e185eda5078fc5
lswjkllc <lswjkllc@163.com>
2024-07-19 14:49:11 +0800

    feature: add sum arrays function

--- cuda/1_check_dimension.cu
+++ cuda/1_check_dimension.cu
@@ -1,12 +1,22 @@
 /*
  * 1_check_dimension.cu
+ *
+ * grid.x 2 grid.y 1 grid.z 1
+ * block.x 3 block.y 1 block.z 1
+ *
+ * threadIdx:(0,0,0) blockIdx:(0,0,0) blockDim:(3,1,1)  gridDim(2,1,1)
+ * threadIdx:(1,0,0) blockIdx:(0,0,0) blockDim:(3,1,1)  gridDim(2,1,1)
+ * threadIdx:(2,0,0) blockIdx:(0,0,0) blockDim:(3,1,1)  gridDim(2,1,1)
+ * threadIdx:(0,0,0) blockIdx:(1,0,0) blockDim:(3,1,1)  gridDim(2,1,1)
+ * threadIdx:(1,0,0) blockIdx:(1,0,0) blockDim:(3,1,1)  gridDim(2,1,1)
+ * threadIdx:(2,0,0) blockIdx:(1,0,0) blockDim:(3,1,1)  gridDim(2,1,1)
  */
 #include <cuda_runtime.h>
 #include <stdio.h>
 
 __global__ void checkIndex(void) {
-  printf("threadIdx:(%d,%d,%d) blockIdx:(%d,%d,%d) blockDim:(%d,%d,%d)\
-  gridDim(%d,%d,%d)\n",
+  printf("threadIdx:(%d,%d,%d) blockIdx:(%d,%d,%d) blockDim:(%d,%d,%d) "
+         "gridDim(%d,%d,%d)\n",
          threadIdx.x, threadIdx.y, threadIdx.z, blockIdx.x, blockIdx.y,
          blockIdx.z, blockDim.x, blockDim.y, blockDim.z, gridDim.x, gridDim.y,
          gridDim.z);
